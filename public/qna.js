e67ac6c4d0b23f5724cf244cb4cca18ca069cdfb
jjori.master <jjori.master@gmail.com>
2018-10-07 18:43:49 +0900

    추가 로그인/로그아웃 기능 추가

--- public/qna.js
+++ public/qna.js
@@ -1,3 +1,5 @@
+'use strict';
+
 function $(selector) {
     return document.querySelector(selector);
 }
@@ -18,11 +20,77 @@ function appendAnswer({content, writer, date, answerId}) {
     return commentHTML;
 }
 
+// 로그아웃 이벤트 리스너
+function logoutListener(evt) {
+    const loginButtonEl = evt.currentTarget;
+
+    const myHeaders = new Headers();
+    myHeaders.append('Accept', 'application/json');
+    myHeaders.append('Content-Type', 'application/json');
+
+    const myInit = {
+        method: 'DELETE',
+        headers: myHeaders,
+        body: JSON.stringify( {command:'deletesession'})
+    };
+
+    fetch('/api/session', myInit).then((res) => {
+        if(res.ok) {
+            return res.json();
+        }
+        throw 'fail logout!!';
+    }).then((json) => {
+        if(json.result === 'ok') {
+            loginButtonEl.innerText = 'LOGIN';
+            loginButtonEl.removeEventListener('click', logoutListener);
+            loginButtonEl.addEventListener('click', loginListener);
+            return false;
+        }
+        throw 'fail logout!!';
+    }).catch((err) => {
+        alert(err.message);
+    });
+};
+
+function loginListener(evt) {
+    const loginButtonEl = evt.currentTarget;
+
+    const myHeaders = new Headers();
+    myHeaders.append('Accept', 'application/json');
+    myHeaders.append('Content-Type', 'application/json');
+
+    const myInit = {
+        method: 'POST',
+        headers: myHeaders,
+        body: JSON.stringify({user: 'jjori.master'})
+    };
+
+    fetch('/api/login', myInit).then((res) => {
+        if(res.ok) {
+            return res.json();
+        }
+        throw 'fail login!!';
+    }).then((json) => {
+        if(json.login === 'ok') {
+            loginButtonEl.innerText = 'LOGOUT';
+            loginButtonEl.removeEventListener('click', loginListener);
+            loginButtonEl.addEventListener('click', logoutListener);
+            return false;
+        }
+        throw 'fail login!!';
+    }).catch((err) => {
+        alert(err.description);
+    });
+}
 
 function initEvents() {
     //이벤트등록
+
+    // 세션 상태 확인??
+    const loginButtonEl = document.querySelector('header .login-btn');
+    loginButtonEl.addEventListener('click', loginListener);
 }
 
 document.addEventListener("DOMContentLoaded", () => {
     initEvents();
-})
+});
