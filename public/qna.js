0afb20fc02cdf1796a3bb32f2d059c26e76e6474
Nam Won Hyung <nwh0526@gmail.com>
2018-10-09 17:35:36 +0900

    update callback 파라미터 받아서 처리하도록 변경

--- public/qna.js
+++ public/qna.js
@@ -2,7 +2,7 @@ function $(selector) {
     return document.querySelector(selector);
 }
 
-function appendAnswer({content, writer, date, answerId}) {
+function appendReply({content, writer, date, answerId}) {
     const commentHTML = `
     <li class="answer" data-id=${answerId}>
         <div class="answer-content"> ${content} </div>
@@ -15,15 +15,16 @@ function appendAnswer({content, writer, date, answerId}) {
         </div>
     </li> `
 
-    return commentHTML;
+    const base = $('ul.answers');
+    base.innerHTML = `${base.innerHTML} ${commentHTML}`;
 }
 
 // 이벤트등록
 function initEvents() {
     addLoginEvent();
     addLogoutEvent();
-    addReplyAddEvent();
-    addReplyDeleteEvent();
+    addReplyAppendEvent();
+    // addReplyRemoveEvent();
 }
 
 function addLoginEvent() {
@@ -36,23 +37,21 @@ function addLoginEvent() {
             method: 'post',
             credentials: 'same-origin',
             headers: {'Content-Type': 'application/json'},
-            content: content
+            content: content,
+            callback: loginCallback
         })
 
-        fetchManagerAsync(clientData)
-            .then(checkHttpStatus)
-            .then(getJson)
-            .then(({login}) => { 
-                if(login === 'ok') {
-                    alert('로그인 되었습니다.');
-                    $('.login-btn').innerText = 'LOGOUT';
-                } else alert('로그인에 실패 하였습니다.');
-                
-                return fetchLogging('login');
-            })
-            .then(response => console.log(response))
-            .catch(error => alert('에러가 발생하였습니다 : ' + error));
+        fetchManager(clientData);
     });
+}
+
+function loginCallback({login}) {
+    if(login === 'ok') {
+        alert('로그인 되었습니다.');
+        $('.login-btn').innerText = 'LOGOUT';
+    } else alert('로그인에 실패 하였습니다.');
+    
+    return fetchLogging('login');
 }
 
 function addLogoutEvent() {
@@ -65,25 +64,24 @@ function addLogoutEvent() {
             method: 'delete',
             credentials: 'same-origin',
             headers: {'Content-Type': 'application/json'},
-            content: content
+            content: content,
+            callback: logoutCallback
         })
 
-        fetchManagerAsync(clientData)
-            .then(checkHttpStatus)
-            .then(getJson)
-            .then(({result}) => { 
-                if(result === 'ok') {
-                    alert('로그아웃 되었습니다.');
-                    $('.login-btn').innerText = 'LOGIN';
-                } else alert('로그아웃에 실패 하였습니다.');
-        
-                return fetchLogging('logout');
-            })
-            .catch(error => alert('에러가 발생하였습니다 : ' + error))
+        fetchManager(clientData);
     });
 }
 
-function addReplyAddEvent() {
+function logoutCallback({result}) {
+    if(result === 'ok') {
+        alert('로그아웃 되었습니다.');
+        $('.login-btn').innerText = 'LOGIN';
+    } else alert('로그아웃에 실패 하였습니다.');
+
+    return fetchLogging('logout');
+}
+
+function addReplyAppendEvent() {
     $('input[type="button"]').addEventListener('click', evt => {
         const inputText = $('textarea.form-control').value;
         const content = Object.assign({}, {content: inputText});
@@ -92,87 +90,13 @@ function addReplyAddEvent() {
             method: 'post',
             credentials: 'same-origin',
             headers: {'Content-Type': 'application/json'},
-            content: content
+            content: content,
+            callback: appendReply
         })
 
-        fetchManagerAsync(clientData)
-            .then(checkHttpStatus)
-            .then(getJson)
-            .then(appendAnswer)
-            .then(reply => {
-                addReply('ul.answers', reply);
-                clearTextArea('textarea.form-control');
-
-                return fetchLogging('add reply');
-            })
-            .catch(error => alert('에러가 발생하였습니다 : \n' + error));
+        fetchManagerAsync(clientData);
+        clearTextArea('textarea.form-control');
     });
-}
-
-function addReplyDeleteEvent() {
-    $('input[type="button"]').addEventListener('click', evt => {
-        const inputText = $('textarea.form-control').value;
-        const content = Object.assign({}, {content: inputText});
-        const clientData = Object.assign({}, {
-            url: 'http://127.0.0.1:3000/api/questions/1/answers',
-            method: 'post',
-            credentials: 'same-origin',
-            headers: {'Content-Type': 'application/json'},
-            content: content
-        })
-
-        fetchManagerAsync(clientData)
-            .then(checkHttpStatus)
-            .then(getJson)
-            .then(appendAnswer)
-            .then(reply => {
-                addReply('ul.answers', reply);
-                clearTextArea('textarea.form-control');
-
-                return fetchLogging('add reply');
-            })
-            .catch(error => alert('에러가 발생하였습니다 : \n' + error));
-    });
-}
-
-function fetchManager({url, method, headers, content, callback}) {
-    return fetch(url, {
-        method: method,
-        headers: headers,
-        body: JSON.stringify(content),
-        // TODO : callback 값 활용. 어떻게(?)
-        callback: callback
-    })
-}
-
-async function fetchManagerAsync({url, method, headers, content, callback}) {
-    return await fetch(url, {
-        method: method,
-        headers: headers,
-        body: JSON.stringify(content),
-        // TODO : callback 값 활용. 어떻게(?)
-        callback: callback
-    })
-}
-
-function fetchLogging(type) {
-    const content = Object.assign({}, {logType: type});
-    return fetch('http://127.0.0.1:3000/api/logging', {
-        method: 'post',
-        credentials: 'same-origin',
-        headers: {'Content-Type': 'application/json'},
-        body: JSON.stringify(content)
-    })
-}
-
-function checkHttpStatus(response) {
-    const {status, statusText} = response;
-    if(status >= 200 && status < 300) return Promise.resolve(response);
-    else return Promise.reject(new Error(statusText));
-}
-
-function getJson(response) {
-    return response.json();
 }
 
 function clearTextArea(selector) {
@@ -183,6 +107,54 @@ function addReply(selector, html) {
     const base = $(selector);
     const temp = `${base.innerHTML} ${html}`; 
     base.innerHTML = temp;
+}
+
+function fetchManager({url, method, headers, content, callback}) {
+    return fetch(url, {
+        method: method,
+        headers: headers,
+        body: JSON.stringify(content),
+    })
+    .then(checkHttpStatus)
+    .then(getJson)
+    .then(callback)
+    .catch(error => alert('에러가 발생하였습니다.' + error));
+}
+
+async function fetchManagerAsync({url, method, headers, content, callback}) {
+    return await fetch(url, {
+        method: method,
+        headers: headers,
+        body: JSON.stringify(content),
+    })
+    .then(checkHttpStatus)
+    .then(getJson)
+    .then(callback)
+    .catch(error => alert('에러가 발생하였습니다.' + error));
+}
+
+function fetchLogging(type) {
+    const content = Object.assign({}, {logType: type});
+    return fetch('http://127.0.0.1:3000/api/logging', {
+        method: 'post',
+        credentials: 'same-origin',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify(content)
+    })
+    .then(checkHttpStatus)
+    .then(getJson)
+    .then(({loggin}) => console.log('loggin :', loggin))
+    .catch(error => alert('에러가 발생하였습니다.' + error));
+}
+
+function checkHttpStatus(response) {
+    const {status, statusText} = response;
+    if(status >= 200 && status < 300) return Promise.resolve(response);
+    else return Promise.reject(new Error(statusText));
+}
+
+function getJson(response) {
+    return response.json();
 }
 
 document.addEventListener("DOMContentLoaded", () => {
