3e6faa170c7a948a213fc16638f38d6d5f57512d
jjori <eoehtkak@naver.com>
2018-10-11 08:28:19 +0900

    5주차 비동기. 미션. 댓글과 로그인 PR 드립니다. (#1)
    * 추가 gitignore 대상 추가
     - .idea/
    
    * 추가 로그인/로그아웃 기능 추가
    
    * 수정 fetchManager와 rest method별 함수를 통한 반복코드 제거
    
    * 수정 api 통신을 위한  form태그안 input type submit을 button형을 변경
    
    * 추가 답글달기 기능 추가 / Api 통신후 에러처리를 위한 RestError 클래스 생성
    
    * 추가  async/await 동작하는 fetchManagerAsync 함수 추가.
    
    * 추가 fetch 요청이 있을때마다 요청 URL별로 서버에 로깅을 하도록 fetch 요청 보내기.
    
    * 수정 초기화면 답변 주석처리
    
    * 추가 답변 삭제
    
    * 수정 역할에 맞는 변수명으로 교체
     - fetch option에 해당하는 부분 myInit => options
    
    * 수정  객체리터럴안의 arrow 함수를 일반 함수로 변경
    
    * 수정 fetchmanger에서 thenable 하게 처리하도록 수정

--- public/qna.js
+++ public/qna.js
@@ -1,8 +1,32 @@
+'use strict';
+
+const ANSWER_DELETE_BUTTON_EL_NAME = 'btn-answer-delete';
+
+class RestError extends Error {
+    constructor(status, message) {
+        super(message);
+        this.status = status;
+    }
+}
+
 function $(selector) {
     return document.querySelector(selector);
 }
 
+function errorHandler(err) {
+    if (err instanceof RestError) {
+        return alert(err.message);
+    }
+
+    if (err instanceof Error) {
+        console.error(err.message);
+    }
+    alert('웁스~!! 처리가 지연되고 있네요\n담당자에게 문의 부탁드립니다.');
+}
+
 function appendAnswer({content, writer, date, answerId}) {
+
+    // noinspection UnnecessaryLocalVariableJS
     const commentHTML = `
     <li class="answer" data-id=${answerId}>
         <div class="answer-content"> ${content} </div>
@@ -10,19 +34,240 @@ function appendAnswer({content, writer, date, answerId}) {
             <div class="answer-id">${writer.id}</div>
             <div class="answer-date">${date}</div>
             <div class="answer-util">
-                <a class="answer-delete" href="/api/questions/2/answers/${answerId}">삭제</a>
+                <button name="btn-answer-delete" class="answer-delete">삭제</button>
             </div>
         </div>
-    </li> `
+    </li> `;
 
     return commentHTML;
 }
 
+const REST = {
+    get(url, callback) {
+        fetchManager(url, 'GET', null, callback);
+    },
+
+    post(url, body, callback) {
+        fetchManager(url, 'POST', body, callback);
+    },
+
+    put(url, body, callback) {
+        fetchManager(url, 'PUT', body, callback);
+    },
+
+    del(url, body, callback) {
+        fetchManager(url, 'DELETE', body, callback);
+    }
+};
+
+function fetchManager(url, method, body, callback) {
+    const headers = new Headers();
+    headers.append('Accept', 'application/json');
+    headers.append('Content-Type', 'application/json');
+
+    const options = {
+        method: method,
+        headers: headers,
+        body: JSON.stringify(body)
+    };
+
+    return fetch(url, options).then((res) => {
+        if (!res.ok) {
+            let message;
+            switch (res.status) {
+                case 401:
+                    message = '로그인 후 다시 시도해 주시기 바랍니다.';
+                    break;
+                default:
+                    message = '처리도중 에러가 발생되었습니다.'
+            }
+
+            throw new RestError(res.status, message);
+        }
+
+        return res.json();
+    }).then((json) => {
+        callback(json);
+    }).then(() => {
+        return asyncLogging(url, method, body);
+    }).then((response) => {
+        if (response.loggin === 'ok') {
+            console.log('logging success!!');
+        }
+    }).catch(errorHandler);
+}
+
+async function fetchManagerAsync(url, method, body, callback) {
+    const headers = new Headers();
+    headers.append('Accept', 'application/json');
+    headers.append('Content-Type', 'application/json');
+
+    const options = {
+        method: method,
+        headers: headers,
+        body: JSON.stringify(body)
+    };
+
+    const res = await fetch(url, options);
+
+    if (!res.ok) {
+        let message;
+        switch (res.status) {
+            case 401:
+                message = '로그인 후 다시 시도해 주시기 바랍니다.';
+                break;
+            default:
+                message = '처리도중 에러가 발생되었습니다.'
+        }
+
+        throw new RestError(res.status, message);
+    }
+
+    const json = await res.json();
+    callback(json);
+
+    const response = await asyncLogging(url, method, body);
+    if (response.loggin === 'ok') {
+        console.log('logging success!!');
+    }
+}
+
+const AsyncREST = {
+    get(url, callback) {
+        fetchManagerAsync(url, 'GET', null, callback).catch(errorHandler);
+    },
+
+    post(url, body, callback) {
+        fetchManagerAsync(url, 'POST', body, callback).catch(errorHandler);
+    },
+
+    put(url, body, callback) {
+        fetchManagerAsync(url, 'PUT', body, callback).catch(errorHandler);
+    },
+
+    del(url, body, callback) {
+        fetchManagerAsync(url, 'DELETE', body, callback).catch(errorHandler);
+    }
+};
+
+async function asyncLogging(url, method, body) {
+    const headers = new Headers();
+    headers.append('Accept', 'application/json');
+    headers.append('Content-Type', 'application/json');
+
+    const options = {
+        method: method,
+        headers: headers,
+        body: JSON.stringify(body)
+    };
+
+    return await fetch(url, options);
+}
+
+// 로그아웃 이벤트 리스너
+function logoutListener(evt) {
+    REST.del('/api/session',
+        {command: 'deletesession'},
+        (json) => {
+            if (json.result === 'ok') {
+                evt.target.innerText = 'LOGIN';
+                evt.target.removeEventListener('click', logoutListener);
+                evt.target.addEventListener('click', loginListener);
+                return false;
+            }
+
+            throw new RestError(500, '로그아웃에 실패 하였습니다.\n재시도 해주세요');
+        });
+}
+
+// 로그인 이벤트 리스너
+function loginListener(evt) {
+    REST.post('/api/login',
+        {user: 'jjori.master'},
+        (json) => {
+            if (json.login === 'ok') {
+                evt.target.innerText = 'LOGOUT';
+                evt.target.removeEventListener('click', loginListener);
+                evt.target.addEventListener('click', logoutListener);
+                return false;
+            }
+
+            throw new RestError(401, '로그인 실패');
+        });
+}
+
+const answers = [];
+
+function resetAnswerList() {
+    // noinspection UnnecessaryLocalVariableJS
+    const answersHtml = answers.map((answer) => {
+        return appendAnswer(answer);
+    }).join('');
+
+    $('ul.answers').innerHTML = answersHtml;
+}
+
+// 답글 달기 로그인상태가 아닐경우 status 401반환
+function answerListener() {
+    const contentEl = $('.answer-form .form-control');
+    const content = contentEl.value;
+
+    if (!content) {
+        return alert('답변은 빈값이 될수 없습니다.!!');
+    }
+
+    AsyncREST.post('/api/questions/1/answers',
+        {content: content},
+        (json) => {
+            if (json['error']
+                && json['error'] === 400) {
+                throw new RestError(400, '답변은 빈값이 될수 없습니다.!!');
+            }
+            answers.push(json);
+
+            resetAnswerList();
+
+            contentEl.value = '';
+        });
+}
+
+function answerDelegationListener(evt) {
+    if (evt.target.name === ANSWER_DELETE_BUTTON_EL_NAME) {
+        const answerId = evt.currentTarget.children[0].dataset.id;
+
+        AsyncREST.del('/api/questions/1/answers/' + answerId,
+            null,
+            (json) => {
+                console.log('delete result is ', json);
+
+                // 삭제 하기
+                const deleteTarget = answers.filter((answer) => {
+                    return answer.answerId === answerId;
+                })[0];
+
+                const targetIndex = answers.indexOf(deleteTarget);
+                answers.splice(targetIndex, 1);
+
+                resetAnswerList();
+
+            });
+    }
+}
 
 function initEvents() {
     //이벤트등록
+
+    // 로그인 이벤트 리스너 등록
+    $('header .login-btn').addEventListener('click', loginListener);
+
+    // 답변하기 이벤트 리스너 등록
+    $('.answer-form .btn').addEventListener('click', answerListener);
+
+    // 답변 삭제 하기
+    $('ul.answers').addEventListener('click', answerDelegationListener)
+
 }
 
 document.addEventListener("DOMContentLoaded", () => {
     initEvents();
-})
+});
